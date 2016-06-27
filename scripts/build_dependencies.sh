2df09bd1e0055c204ab0be37ab3cfa9096771d26
Zogg <zoggified@gmail.com>
2016-06-28 00:49:26 +0300

    vagrant dependencies

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-### MESOS
+### Kubernetes
 
-yum remove -y subversion subversion-libs
 yum install -y tar git
-
-curl -skO /etc/yum.repos.d/epel-apache-maven.repo \
-  'http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo'
-
 yum install -y epel-release
-
-cat << EOF >> /etc/yum.repos.d/wandisco-svn.repo
-[WANdiscoSVN]
-name=WANdisco SVN Repo 1.9
-enabled=1
-baseurl=http://opensource.wandisco.com/centos/7/svn-1.9/RPMS/x86_64/
-gpgcheck=1
-gpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
-EOF
-
 yum update -y systemd
-yum install -y apache-maven python-devel java-1.8.0-openjdk-devel zlib-devel \
-               libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 \
-               apr-devel subversion-devel apr-util-devel maven
+yum install -y docker
+#sudo service docker start
+
+# Install newer go Version
+sudo -u vagrant bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) && \
+source /home/vagrant/.gvm/scripts/gvm && \
+gvm install go1.4 && \
+gvm use go1.4 && \
+export GOROOT_BOOTSTRAP=$GOROOT && \
+gvm install go1.6
+
+chmod -R +rwx /home/vagrant/.gvm
+chown -R vagrant /home/vagrant/.gvm

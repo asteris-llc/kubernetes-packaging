851aa1c45e63422be7cf5c4274c44305ce5654c2
ryane <ryanesc@gmail.com>
2016-06-28 15:42:23 -0400

    working k8s build

--- kubernetes/kubernetes/build.sh
+++ kubernetes/kubernetes/build.sh
@@ -5,12 +5,10 @@ INSTALL={{.BuildRoot}}/out
 mkdir $INSTALL
 
 cd {{.BuildRoot}}
-mkdir kubernetes && cd kubernetes
 
 # Fetch a branch with working consul-integration
-git init
-git remote add -t consul-integration -f origin https://github.com/asteris-llc/kubernetes.git
-git checkout consul-integration
+git clone -q -b consul-integration --single-branch https://github.com/asteris-llc/kubernetes.git
+cd kubernetes
 
 # Build all components
 # If we are running inside vagrant...
@@ -30,5 +28,11 @@ cp _output/dockerized/bin/linux/amd64/kubelet _output/dockerized/bin/linux/amd64
 # Build hypercube docker image
 cd {{.BuildRoot}}/kubernetes
 pushd ./cluster/images/hyperkube/
-ARCH=amd64 REGISTRY="ciscocloud" make push VERSION=v{{.Version}} || echo "hypercube push failed" && sleep 1
+
+# hack to workaround sed syntax fixed in https://github.com/kubernetes/kubernetes/commit/cb11324cc32ffc58c022daef3278a0810c675778
+sed -i "s/sed -i \"\"/sed -i/g" Makefile
+
+ARCH=amd64 REGISTRY="ciscocloud" make VERSION=v{{.Version}} || echo "hypercube push failed" && sleep 1
 popd
+
+sudo rm -rf _output
--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -2,14 +2,25 @@
 
 ### Kubernetes
 
-yum install -y tar git
-yum install -y epel-release
-yum update -y systemd
-yum install -y docker
-sudo service docker start
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum update -y
+sudo yum install -y tar git
+sudo yum install -y epel-release
+sudo yum update -y systemd
+sudo yum install -y docker-engine
 sudo groupadd docker
 sudo usermod -a -G docker vagrant
 sudo usermod -a -G dockerroot vagrant
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Install newer go Version
 sudo -u vagrant bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer) && \
@@ -21,3 +32,9 @@ gvm install go1.6
 
 chmod -R +rwx /home/vagrant/.gvm
 chown -R vagrant /home/vagrant/.gvm
+
+export GOPATH=/home/vagrant/go/
+go get -u github.com/asteris-llc/hammer
+
+sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+sudo reboot

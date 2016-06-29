ab78a7b34c6f0fdda032268f68b8b699be8476f1
ryane <ryanesc@gmail.com>
2016-06-28 22:04:43 -0400

    kubernetes build

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+cat scripts/images
+docker images
+
 while read image; do
     docker images | grep "$image" | awk '{printf "%s:%s\n", $1, $2}' | xargs -r -n1 docker push
 done < scripts/images
